15038d84d41eea342808db76f555417c345c0e45
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-29 18:08:53 +0800

    [feat] init user order interfacial

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Order from './views/Order.vue';
 Vue.use(Router);
 
 export default new Router({
   mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'Order',
+      component: Order
+    },
   ]
 });
---
+++ src/views/Order.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="order">
+    <van-cell-group>
+      <van-field label="车牌号" placeholder="请输入车牌号" v-model="carNum"/>
+      <van-field label="地点" placeholder="请输入停车地点" v-model="address"/>
+      <van-field label="预约时间" placeholder="请输入预约时间" v-model="oppointTime"/>
+      <van-button type="info">下单</van-button>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      carNum:'',
+      address:'',
+      oppointTime:''
+    };
+  },
+  methods:{
+
+  }
+};
+</script>
+
