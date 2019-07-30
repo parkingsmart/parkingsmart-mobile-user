62c1a727d8b657b1a7c408f09a11b7ae3560ac56
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-30 09:09:39 +0800

    [feat] add appiont date picker

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -3,6 +3,21 @@
     <van-cell-group>
       <van-field label="车牌号" placeholder="请输入车牌号" v-model="carNum" />
       <van-field label="预约地点" placeholder="请输入停车地点" v-model="address" />
+      <van-field
+        @click="show=true"
+        label="预约时间"
+        placeholder="请选择停车时间"
+        v-model="currentTime"
+        :readonly="true"
+      />
+      <van-datetime-picker
+        @confirm="show=false"
+        v-show="show"
+        v-model="currentTime"
+        type="time"
+        :min-hour="minHour"
+        :max-hour="23"
+      />
       <van-button type="info" @click="creatOrder" size="large">下单</van-button>
       <van-toast id="van-toast" />
     </van-cell-group>
@@ -16,12 +31,15 @@ export default {
     return {
       carNum: "",
       address: "",
-      appointTime: ""
+      appointTime: "",
+      currentTime: "",
+      show: false,
+      minHour: new Date().getHours() - 1
     };
   },
   methods: {
     creatOrder() {
-      if ( this.carNum.trim() === "" || this.address.trim() === "" ) {
+      if (this.carNum.trim() === "" || this.address.trim() === "") {
         this.$notify("请把信息填写完整");
         return;
       }
@@ -31,12 +49,22 @@ export default {
         userId: 1,
         carNumber: this.carNum,
         appointAddress: this.address,
-        appointTime: date,
+        appointTime: this.dateConverter(),
         createAt: date,
         status: 0
       };
       OrderApi.addOrder(order);
       this.$toast("下单成功");
+    },
+
+    dateConverter() {
+      let date = new Date();
+      let Y = date.getFullYear();
+      let M = date.getMonth() + 1;
+      let D = date.getDate();
+      let timestr = `${Y}-0${M}-${D} ${this.currentTime}:00:000`;
+      let timestamp = new Date(timestr).getTime();
+      return timestamp;
     }
   }
 };
