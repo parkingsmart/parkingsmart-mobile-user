23565949b1b29ac5dae141deed8932f9974775d5
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-31 22:19:18 +0800

    [feat] add pay password function

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -4,7 +4,7 @@
       <van-cell title="我的账号" v-model="username" />
       <van-cell title="我的积分" v-model="integral" label="积分满20，购物享8.8折" />
       <van-collapse v-model="activeNames">
-        <van-collapse-item title="设置">
+        <van-collapse-item title="修改密码">
           <van-cell-group>
             <van-field
               label="旧密码"
@@ -35,6 +35,30 @@
             </div>
           </van-cell-group>
         </van-collapse-item>
+
+        <van-collapse-item title="设置支付密码">
+          <van-cell-group>
+            <van-field
+              label="支付密码"
+              @blur="check('payPassword')"
+              :error-message="payPassword.err"
+              v-model="form.payPassword"
+              placeholder="请输入6位数支付密码"
+              type="payPassword"
+            />
+            <van-field
+              label="确认密码"
+              @blur="check('comfirmPayPwd')"
+              :error-message="comfirmPayPwd.err"
+              v-model="form.comfirmPayPwd"
+              placeholder="请确认密码"
+              type="comfirmPayPwd"
+            />
+            <div class="save">
+              <van-button class="passwd" @click="addPayPassword">保存</van-button>
+            </div>
+          </van-cell-group>
+        </van-collapse-item>
       </van-collapse>
     </van-cell-group>
   </div>
@@ -50,7 +74,9 @@ export default {
       form: {
         password: "",
         comfirmPwd: "",
-        oldPassword: ""
+        oldPassword: "",
+        payPassword: "",
+        comfirmPayPwd: ""
       },
       oldPassword: {
         err: "",
@@ -61,6 +87,14 @@ export default {
         text: "请输入新密码"
       },
       comfirmPwd: {
+        err: "",
+        text: "请再次确认密码"
+      },
+      payPassword: {
+        err: "",
+        text: "请输入6位数密码"
+      },
+      comfirmPayPwd: {
         err: "",
         text: "请再次确认密码"
       }
@@ -86,6 +120,12 @@ export default {
         this[item].err = this.form[item].trim() === "" ? this[item].text : "";
       }
     },
+    async addPayPassword() {
+      await UserApi.addPayPassword(this.$store.getters.id, {
+        payPassword: this.form.payPassword
+      });
+    },
+
     async updatePassword() {
       let vaild = true;
       for (const key in this.form) {
@@ -101,7 +141,10 @@ export default {
           return;
         }
         const res = await RequestHandler.invoke(
-          UserApi.updatePassword(this.$store.getters.id, {oldPassword:this.form.oldPassword,password:this.form.password})
+          UserApi.updatePassword(this.$store.getters.id, {
+            oldPassword: this.form.oldPassword,
+            password: this.form.password
+          })
         )
           .msg("修改成功")
           .exec();
